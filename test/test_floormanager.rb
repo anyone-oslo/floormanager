d39d958d5d16da2731fe4c9290698720207daa15
Inge Jørgensen <inge@manualdesign.no>
2011-06-17 19:24:29 +0200

    Basic tests

--- test/test_floormanager.rb
+++ test/test_floormanager.rb
@@ -1,7 +1,32 @@
 require 'helper'
 
 class TestFloorManager < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+
+	context "a processed queue" do
+		setup do
+			workers = FloorManager::Workers.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+			@results = workers.perform(:threads => 5) do |number|
+				number * 10
+			end
+		end
+
+		should "process a queue" do
+			@results.each do |original, result|
+				assert_equal (original * 10), result
+			end
+		end
+	end
+
+	context 'a queue with non-unique items' do
+		setup do
+			@queue = FloorManager::Queue.new(['a', 'a', 'b', 'c'])
+		end
+		
+		should "not hang" do
+			FloorManager::Workers.new(@queue).perform(:threads => 5) do |item|
+			end
+			assert true # The processes will simply hang here if not
+		end
+	end
+
 end
