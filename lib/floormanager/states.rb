1ee5db8704a5aef536c23f0647ad5ad22b49cb3d
Inge Jørgensen <inge@manualdesign.no>
2012-09-24 14:09:21 +0200

    Converted tabs to spaces

--- lib/floormanager/states.rb
+++ lib/floormanager/states.rb
@@ -1,8 +1,8 @@
 module FloorManager
-	module States
-		PENDING     = 0
-		CHECKED_OUT = 1
-		FAILED      = 2
-		SUCCESS     = 3
-	end
+  module States
+    PENDING     = 0
+    CHECKED_OUT = 1
+    FAILED      = 2
+    SUCCESS     = 3
+  end
 end
